f882fc6b937f194d9251def4bbc4ab1a655cb80c
raphael <raphael@Ubuntu.myguest.virtualbox.org>
2024-05-09 22:14:23 -0300

    feat(data): setup flyway

---
+++ src/main/resources/migrations/V2__create_courses_table.sql
@@ -0,0 +1,10 @@
+CREATE TABLE IF NOT EXISTS `courses` (
+    `id` INTEGER PRIMARY KEY AUTO_INCREMENT,
+    `code` VARCHAR(10) NOT NULL UNIQUE,
+    `name` VARCHAR(50) NOT NULL,
+    `description` VARCHAR(100),
+    `status` VARCHAR(7) NOT NULL,
+    `creation_date` DATE,
+    `deactivate_date` DATE,
+    `instructor_id` INTEGER
+);
