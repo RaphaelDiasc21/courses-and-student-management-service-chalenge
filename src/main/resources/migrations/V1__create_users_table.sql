9a93a745180af3d8ffbc53f00b152b2ae2f9c0ec
RaphaelDiasc21 <raphaeldiasc16@gmail.com>
2024-05-15 11:48:34 -0300

    refact(data): refactor role length for database

--- src/main/resources/migrations/V1__create_users_table.sql
+++ src/main/resources/migrations/V1__create_users_table.sql
@@ -4,6 +4,6 @@ CREATE TABLE IF NOT EXISTS `users` (
     `username` VARCHAR(20) NOT NULL UNIQUE,
     `email` VARCHAR(100) NOT NULL UNIQUE,
     `password` VARCHAR(100) NOT NULL,
-    `role` VARCHAR(9) NOT NULL,
+    `role` VARCHAR(10) NOT NULL,
     `creation_date` DATE
 );
